59533f04dcbcff320e84ecb7b202a3ed3b6fb7ee
Mattias Pfeiffer <mattias@pfeiffer.dk>
2011-10-06 14:47:58 +0200

    Add configuration based on Rails.env

--- init.rb
+++ init.rb
@@ -12,10 +12,23 @@ ElasticSearch::Client.class_eval do
   include Escargot::AdminIndexVersions
 end
 
-unless File.exists?(Rails.root + "/config/elasticsearch.yml")
-  Rails.logger.warn "No config/elastic_search.yaml file found, connecting to localhost:9200"
+config_file = Rails.root.join("config", "elasticsearch.yml")
+
+unless File.exists?(config_file)
+  Rails.logger.debug "No config/elastic_search.yaml file found, connecting to localhost:9200"
   $elastic_search_client ||= ElasticSearch.new("http://localhost:9200", :timeout => 2)
 else
-  config = YAML.load_file(Rails.root + "/config/elasticsearch.yml")
-  $elastic_search_client ||= ElasticSearch.new(config["host"] + ":" + config["port"].to_s, :timeout => 20)
+  config = YAML.load_file(config_file)
+  
+  if config = config[Rails.env]
+    host = config["host"]
+    port = config["port"]
+    timeout = config["timeout"]
+  end
+  
+  host    ||= "localhost"
+  port    ||= "9200"
+  timeout ||= 20
+  
+  $elastic_search_client ||= ElasticSearch.new("#{host}:#{port}", :timeout => timeout)
 end
